2b6103c1be0fd2d279b190a363fc1b0d658e1233
Robertsilva2024 <robertsilvastar2023@gmail.com>
2024-08-13 16:49:59 -0300

    Update Conta Bancaria.py

--- Conta Bancaria.py
+++ Conta Bancaria.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 # menu de opções
 print('Bem vindo!,Ao')
 print('{:=^31}'.format('|_Aplicativo_|'))
@@ -37,25 +36,20 @@ elif(op != op5):
 opcao1 = float(input('digite o valor que será depositado:'))
 opcao2 = float(input('digite o valor a ser sacado:'))
 opcao3 = float(input('digite o valor do pix para sua conta'))
+
+print('Temos Recarga da: Tim,Vivo,Claro')
 opcao4 = str(input('digite a operadora Escolhida:'))
 opcao5 = float(input('digite a recarga a ser enviada de R$:'))
 print('Recarga Realiza com Sucesso!!')
+print('seu saldo e de:2000')
 
-deposito = opcao1 + opcao2
-saque = opcao2 - 2000
-pix = opcao1 + opcao3
-recarga = opcao4
-saldoanterior = 2000
-saldoatual = opcao1 + opcao3
-saldosobrado = opcao1 + opcao3 - opcao2 - opcao5
-
-deposito = opcao1
-saque = 2000 - opcao2
-pix = opcao3
+deposito = 2000 + opcao1
+saque = deposito - opcao2
+pix = saque + opcao3
 recarga = opcao5
 saldoanterior = 2000
-saldoatual = saldoanterior + opcao1 - opcao2 + opcao3 + opcao5
-saldosobrado = saldoatual
+saldoatual =  2000 + opcao1 + opcao3 - opcao2
+saldosobrado = saldoatual - opcao2 - opcao5
 
 # condições a serem feitas
 if (opcao1 > 0):
@@ -76,7 +70,7 @@ print('O Deposito foi de R$',opcao1,'Reias e aumentou para R$:',deposito)
 print('E O saque foi de R$',opcao2,'Reais e Diminuil para R$',saque)
 print('já o pix foi de R$',opcao3,'Reais e aumentou para R$',pix)
 print('A Recarga Feita atualmente foi de',opcao5,'Reais')
-print('A opção escolhida de Recarga foi A:',recarga,'\n')
+print('A opção escolhida de Recarga foi A:',opcao4,'\n')
 print('Saldo anterior foi de R$:',saldoanterior,'mil Reias')
 print('O Saldo atual é de R$',saldoatual)
 print('E o Resto que Sobrou foi de R$:',saldosobrado,'Reias','\n')
@@ -91,9 +85,9 @@ comprorecarga = opcao5
 print('Deposito atual foi de R$',comprodeposito,'Reias')
 print('O Saque Feito atualmente foi de R$',comprosaque,'Reais')
 print('O Comprovante do Pix foi de R$',compropix,'Reais')
-print('E Por ultimo a Recarga que foi de',opcao5,'Reais','na',recarga)
+print('E Por ultimo a Recarga que foi de',opcao5,'Reais','na',opcao4)
 
-# Grafico matplotib em Barras
+import matplotlib.pyplot as plt
 opcoesconta = ['Deposito','Saque','Pix','Recarga',]
 valores = [opcao1,opcao2,opcao3,opcao5]
 
@@ -111,4 +105,6 @@ plt.title('Resumo Financeiro')
 plt.xlabel('Extrato do Mês:')
 plt.ylabel('Valores'.upper())
 plt.show()
+
+
 
